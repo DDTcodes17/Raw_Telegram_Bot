1a98ed36dc8582f10f5ed33ea3fb51a689eda26b
Dhruv Tiwari <tiwaridhruv15@gmail.com>
2026-09-20 13:12:15 +0530

    Added Memoey

--- agents.py
+++ agents.py
@@ -1,13 +1,12 @@
 from langchain.agents import create_agent
 from llm import gemini_llm
 from tools import live_cricket_score
+from langgraph.checkpoint.memory import InMemorySaver
 
 gemini_agent = create_agent(
     model=gemini_llm,
     tools=[live_cricket_score],
-    system_prompt="You are a helpful sport Assistant"
+    system_prompt="You are a helpful sport Assistant",
+    checkpointer= InMemorySaver()
 )
 
-response = gemini_agent.invoke({"messages":{"role": "user", "content": "What is current cricket score between India  and pakistan"}})
-
-print(response['messages'][-1].content[0]['text'])
